d212d0775690fc6ee7c8754027c591a14ef56d0d
zagreus <elouahdizaki@gmail.com>
2024-09-30 15:33:48 +0100

    final

--- restaurant/views.py
+++ restaurant/views.py
@@ -4,13 +4,15 @@ from .models import Menu , Booking
 from .serializer import MenuSerializer , BookingSerializer
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 # Create your views here.
+
+def home(request):
+    return render(request, 'index.html')
 @api_view()
 @permission_classes([IsAuthenticated])
 def msg(request):
     return Response({"message":"This view is protected"})
-def home(request):
-    return render(request, 'index.html')
 class MenuItemView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
